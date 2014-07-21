8c6586cdfc17b72a27fbbe6c86d88d7054eaf2e9
DailyEfforts <DailyEfforts@gmail.com>
2014-07-21 23:22:55 +0800

    add Popup anim

--- app/src/com/gmail/dailyefforts/flowchart/picker/PickerPopup.java
+++ app/src/com/gmail/dailyefforts/flowchart/picker/PickerPopup.java
@@ -3,49 +3,56 @@ package com.gmail.dailyefforts.flowchart.picker;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.PopupWindow;
 
 import com.gmail.dailyefforts.flowchart.MainActivity;
 import com.gmail.dailyefforts.flowchart.R;
 
-/**
- * Created by os on 14-7-9.
- */
 public class PickerPopup {
-    private static PickerPopup sInstance = new PickerPopup();
-    private static PopupWindow sPopup = new PopupWindow();
+	private static PickerPopup sInstance = new PickerPopup();
+	private static PopupWindow sPopup = new PopupWindow();
 
-    public static PickerPopup getInstance() {
-        return sInstance;
-    }
+	public static PickerPopup getInstance() {
+		return sInstance;
+	}
 
-    public static void show(View anchor, MainActivity activity) {
-        LayoutInflater inflater = (LayoutInflater) anchor.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        PickerView view = (PickerView)inflater.inflate(R.layout.picker, null);
-        sPopup.dismiss();
-        sPopup.setBackgroundDrawable(new ColorDrawable(Color.argb(60, 0, 255, 0)));
-        sPopup.setWindowLayoutMode(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        sPopup.showAtLocation(anchor, Gravity.LEFT | Gravity.BOTTOM, 0, 0);
-        sPopup.showAsDropDown(anchor, 0, 0);
-        sPopup.setFocusable(true);
-        sPopup.setTouchable(true);
-        sPopup.setContentView(view);
-        view.setLis(activity);
-    }
+	public static void show(View anchor, MainActivity activity) {
+		LayoutInflater inflater = (LayoutInflater) anchor.getContext()
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		PickerView view = (PickerView) inflater.inflate(R.layout.picker, null);
+		sPopup.dismiss();
+		sPopup.setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+		sPopup.setWindowLayoutMode(ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		sPopup.showAtLocation(anchor, Gravity.LEFT | Gravity.BOTTOM, 0, 0);
+		sPopup.setFocusable(true);
+		sPopup.setTouchable(true);
+		sPopup.setContentView(view);
+		sPopup.setAnimationStyle(R.style.PopupAnim);
+		view.setLis(activity);
+		view.setOnTouchListener(new View.OnTouchListener() {
 
-    private PickerPopup() {
-    }
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				sPopup.dismiss();
+				return true;
+			}
+		});
+	}
 
-    public boolean isShowing() {
-        return sPopup.isShowing();
-    }
+	private PickerPopup() {
+	}
 
-    public void dismiss() {
-        sPopup.dismiss();
-    }
+	public boolean isShowing() {
+		return sPopup.isShowing();
+	}
+
+	public void dismiss() {
+		sPopup.dismiss();
+	}
 }
--- app/src/com/gmail/dailyefforts/flowchart/picker/PickerView.java
+++ app/src/com/gmail/dailyefforts/flowchart/picker/PickerView.java
@@ -1,48 +1,63 @@
 package com.gmail.dailyefforts.flowchart.picker;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 
 import com.gmail.dailyefforts.flowchart.MainActivity;
 import com.gmail.dailyefforts.flowchart.R;
 
-public class PickerView extends TableLayout implements View.OnClickListener {
-    private MainActivity mActivity;
-    public PickerView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+public class PickerView extends RelativeLayout implements View.OnClickListener {
+	private MainActivity mActivity;
 
-    public void setLis(MainActivity activity) {
-        View row =  findViewById(R.id.tb);
-        Button btnDecision = (Button) row.findViewById(R.id.btn_decision);
-        Button btnTerminator = (Button) row.findViewById(R.id.btn_terminator);
-        Button btnProcess = (Button) row.findViewById(R.id.btn_process);
-        btnDecision.setOnClickListener(this);
-        btnTerminator.setOnClickListener(this);
-        btnProcess.setOnClickListener(this);
-        mActivity = activity;
-    }
+	public PickerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    @Override
-    public void onClick(View v) {
-        final int id = v.getId();
-        switch (id) {
-            case R.id.btn_terminator:
-                break;
-            case R.id.btn_decision:
-                break;
-            case R.id.btn_process:
-                break;
-            default:
-                break;
-        }
-        if (mActivity != null) {
-            mActivity.onClick(v);
-        }
-        PickerPopup.getInstance().dismiss();
-    }
+	public void setLis(MainActivity activity) {
+		View row = findViewById(R.id.tb);
+		Button btnDecision = (Button) row.findViewById(R.id.btn_decision);
+		Button btnTerminator = (Button) row.findViewById(R.id.btn_terminator);
+		Button btnProcess = (Button) row.findViewById(R.id.btn_process);
+		btnDecision.setOnClickListener(this);
+		btnDecision.setOnTouchListener(new View.OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				DragShadowBuilder shadowBuilder = new DragShadowBuilder(v);
+				ClipData data = ClipData.newPlainText("label", "text111");
+				v.startDrag(data, shadowBuilder, v, 0);
+				PickerPopup.getInstance().dismiss();
+				return true;
+			}
+		});
+		btnTerminator.setOnClickListener(this);
+		btnProcess.setOnClickListener(this);
+		mActivity = activity;
+	}
+
+	@Override
+	public void onClick(View v) {
+		final int id = v.getId();
+		switch (id) {
+		case R.id.btn_terminator:
+			break;
+		case R.id.btn_decision:
+			break;
+		case R.id.btn_process:
+			break;
+		default:
+			break;
+		}
+		if (mActivity != null) {
+			mActivity.onClick(v);
+		}
+		PickerPopup.getInstance().dismiss();
+	}
 }
